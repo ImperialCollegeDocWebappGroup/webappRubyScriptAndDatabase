94afe9e66bec239bf884feaa55b1f87369f37d41
Sam <samlam2012@hotmail.com>
2015-06-20 13:41:42 +0100

    Update server2.rb

--- server2.rb
+++ server2.rb
@@ -11,36 +11,97 @@ def main
   filePath = "/homes/jl6613/public_html/"
   fileName = "serverIp.txt"
   File.open(filePath+fileName, 'w') { |file| file.write(localIp) }
-	@conn = PGconn.connect(
-		:host => 'db.doc.ic.ac.uk',
-		:port => '5432',
-       		:dbname => 'g1427141_u',
-        	:user => 'g1427141_u',
-        	:password => 'BhNt16JkU5')
+  countFileName = "picCount.txt"
+  index = 0
+  if File.exist?(filePath + countFileName)
+    index = File.read(filePath+countFileName)
+  else
+    File.open(filePath+countFileName, 'w') { |file| file.write("0") }
+  end
+  @conn = PGconn.connect(
+    :host => 'db.doc.ic.ac.uk',
+    :port => '5432',
+          :dbname => 'g1427141_u',
+          :user => 'g1427141_u',
+          :password => 'BhNt16JkU5')
   status = @conn.connect_poll
   if status == PG::PGRES_POLLING_OK
     print "connect success!\n"
   else
     abort("connect fail!")
-	end
+  end
   server = TCPServer.new 1111
-  index = 0
+  puts index
   loop {
     puts "new loop"
     client = server.accept
     puts "client accpetd"
     line = client.gets
+    line = line.lstrip.rstrip
     puts line
-    if (line == "SAVE1\n") || (line == "SAVE2\n")
-      cloth = ""
-      if line == "SAVE1\n"
-        cloth = "tops"
+    if (line == "SP")
+      puts "save and publish confirm"
+      client.puts "GOOD"
+      line = client.gets
+      line = line.lstrip.rstrip
+      puts line
+      checkExist = "SELECT EXISTS(SELECT 1 from userprofile WHERE login = '" + line + "');"
+      res  = @conn.exec(checkExist)
+      result = res[0]["exists"] 
+      if result == "t"
+        client.puts "GOOD2"
+        fileName = filePath + "picFile" + index.to_s + ".jpeg"
+        name = line
+        line = client.gets
+        line = line.lstrip.rstrip
+        File.open(fileName, 'wb') do|f|
+          f.write(Base64.decode64(line))
+        end
+        puts "saved"
+        savedName = "picFile" + index.to_s + ".jpeg"
+        puts savedName
+        client.puts "GOOD3"
+        cont = client.gets
+        cont = line.lstrip.rstrip
+        query = "UPDATE publishs SET shows = array_append(shows, ROW('" + cont + "', LOCALTIMESTAMP,ARRAY[]::comment[],'" + savedName + "',0,'')::publishitem) WHERE usrname = '"+name+"';"
+        puts query
+        begin
+          res  = @conn.exec(query)
+          puts res.res_status(res.result_status) 
+        rescue PG::Error => err
+          str = err.result.error_field( PG::Result::PG_DIAG_SEVERITY )
+          puts "Return error!"
+        end
+        if str == "ERROR"
+          puts str
+          client.puts "ERROR"
+          next
+        end 
+        index = Integer(index) + 1
+        File.open(filePath+countFileName, 'w') { |file| file.write(index) }
+        client.puts "SAVEANDPUBLISHDONE"
+        client.close
+        puts "save done, close"
       else
+        puts "save username error"
+        client.puts "ERROR"
+        client.close
+        next
+      end
+    elsif (line == "SAVE1") || (line == "SAVE2") || (line == "SAVE3")
+      cloth = ""
+      if line == "SAVE1"
+        cloth = "tops"
+      elsif line == "SAVE2"
         cloth = "buttoms"
+      else
+        cloth = "wholelook"
       end
       puts "save confirm"
       client.puts "GOOD"
       line = client.gets
+      line = line.lstrip.rstrip
+      puts line
       #SELECT EXISTS(SELECT 1 from userprofile WHERE login = 'Sam');
       #line2 = "SELECT EXISTS(SELECT 1 from userprofile WHERE login = 'Sam2');"
       #puts res[0]["exists"]
@@ -48,12 +109,16 @@ def main
       res  = @conn.exec(checkExist)
       result = res[0]["exists"]
       if result == "t"
-        fileName = filePath + "file" + index.to_s + ".jpeg"
+        client.puts "GOOD2"
+        fileName = filePath + "picFile" + index.to_s + ".jpeg"
+        name = line
         line = client.gets
         File.open(fileName, 'wb') do|f|
           f.write(Base64.decode64(line))
         end
-        query = "UPDATE userprofile SET " + cloth + " = array_append(" + cloth +", " + index.to_s + ") WHERE login = '';"
+        puts "saved"
+        query = "UPDATE userprofile SET " + cloth + " = array_append(" + cloth +", 'pic" + index.to_s + "') WHERE login = '" + name + "';"
+        puts query
         begin
           res  = @conn.exec(query)
           puts res.res_status(res.result_status)
@@ -66,7 +131,8 @@ def main
           client.puts "ERROR"
           next
         end 
-        index += 1
+        index = Integer(index) + 1
+        File.open(filePath+countFileName, 'w') { |file| file.write(index) }
         client.puts "SAVEDONE"
         client.close
         puts "save done, close"
@@ -76,20 +142,72 @@ def main
         client.close
         next
       end
-    elsif line == "PUBLISH\n"
+    elsif line == "PUBLISH"
       #time = Time.now.to_s
       #SELECT CURRENT_TIMESTAMP(2);
       puts "publish confirm"
       client.puts "GOOD"
       line = client.gets
+      puts line
+      line = line.lstrip.rstrip
       checkExist = "SELECT EXISTS(SELECT 1 from userprofile WHERE login = '" + line + "');"
+      puts checkExist
       uname = line
       res  = @conn.exec(checkExist)
       result2 = res[0]["exists"]
+      puts result2
       if result2 == "t"
-        client.puts "GOOD"
-        line = client.gets
-        query = "UPDATE publishs SET shows = array_append(shows, ROW('"+ line+"', LOCALTIMESTAMP,ARRAY[]::comment[],'')::publishitem) WHERE usrname = '"+uname+"';"
+        client.puts "GOOD2"
+        puts "good2"
+        line2 = client.gets
+        puts line2
+        query = "UPDATE publishs SET shows = array_append(shows, ROW('" + line2 + "', LOCALTIMESTAMP,ARRAY[]::comment[],'',0,'')::publishitem) WHERE usrname = '"+uname+"';"
+        begin
+          res  = @conn.exec(query)
+          puts res.res_status(res.result_status)
+        rescue PG::Error => err
+          str = err.result.error_field( PG::Result::PG_DIAG_SEVERITY )
+          puts "Return error!"
+        end
+        if str == "ERROR"
+          puts str
+          client.puts "ERROR"
+          client.close
+          next
+        end 
+        client.puts "PUBLISHDONE"
+        client.close
+        puts "save done, close"
+      else
+        puts "save username error"
+        client.puts "ERROR"
+        client.close
+        next
+      end
+    elsif line == "PUBLISH2"
+      #time = Time.now.to_s
+      #SELECT CURRENT_TIMESTAMP(2);
+      puts "publish2 confirm"
+      client.puts "GOOD"
+      line = client.gets
+      puts line
+      line = line.lstrip.rstrip
+      checkExist = "SELECT EXISTS(SELECT 1 from userprofile WHERE login = '" + line + "');"
+      puts checkExist
+      uname = line
+      res  = @conn.exec(checkExist)
+      result2 = res[0]["exists"]
+      puts result2
+      if result2 == "t"
+        client.puts "GOOD2"
+        puts "good2"
+        line2 = client.gets
+        puts line2
+         client.puts "GOOD3"
+        puts "good3"
+        line3 = client.gets
+        puts line3
+        query = "UPDATE publishs SET shows = array_append(shows, ROW('" + line3 + "', LOCALTIMESTAMP,ARRAY[]::comment[],'',0,'" + line2 + "')::publishitem) WHERE usrname = '"+uname+"';"
         begin
           res  = @conn.exec(query)
           puts res.res_status(res.result_status)
@@ -172,3 +290,8 @@ def main
 end
 
 main
+
+    
+
+
+    
